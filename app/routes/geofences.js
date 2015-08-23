c982453b5c8d9b17719ec215e6e122a3cbb83f63
Tomasz Subik <tsubik@gmail.com>
2015-08-23 18:48:42 +0200

    cordova addon installed

--- app/routes/geofences.js
+++ app/routes/geofences.js
@@ -12,6 +12,6 @@ export default Ember.Route.extend({
             notification: {
                 text: "Geofence 2"
             }
-        }]
+        }];
     }
 });
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-geofence',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    defaultLocationType: 'auto',
     contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self'",
@@ -24,15 +24,20 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    cordova: {
+      rebuildOnChange: false,
+      emulate: false
     }
   };
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
