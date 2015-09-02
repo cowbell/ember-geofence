44f48b8a782f7b5aea5a7099dfcee9b9c0d3a2f3
Tomasz Subik <tsubik@gmail.com>
2015-09-02 19:50:40 +0200

    removing global loading action

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,5 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-  actions: {
-    loading: function(transition, originRoute) {
-      console.log("Loading...");
-      // substate implementation when returning `true`
-      return true;
-    }
-  }
+
 });
